c3047c0d74b87e96decba9eedf72bb2fbaa4fdca
vladnomad <vlad.nmd@gmail.com>
2022-07-27 19:39:10 +0200

    practice 4

--- js/23 + practice 3.js
+++ js/23 + practice 3.js
@@ -36,7 +36,7 @@ first: for (let i = 0; i < 3; i++) {
         }
     }
 }
-// HOMEWORK
+// PRACTICE 3
 
 function firstTask() {
     for (let i = 5; i < 11; i++) {
@@ -90,3 +90,58 @@ function fifthTask() {
     
     return arrayOfNumbers;
 }
+
+// PRACTICE 4
+
+    function firstTask() {
+        const arr = [3, 5, 8, 16, 20, 23, 50]; // 0, 2, 3, 8, 4, 3, 27
+        const result = [];
+
+        for (let i = 0; i < arr.length; i++) {
+            result[i] = arr[i];
+        }
+
+        console.log(result);
+        return result;
+    }
+
+    function secondTask() {
+        const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+        for (let i = 0; i < data.length; i++) {
+            if (typeof(data[i]) === 'number') {
+                data[i] = data[i] * 2;
+            } else if (typeof(data[i]) === 'string') {
+                data[i] = `${data[i]} - done`;
+            }
+        }
+
+        console.log(data);
+        return data;
+    }
+
+    function thirdTask() {
+        const data = [5, 10, 'Shopping', 20, 'Homework'];
+        const result = [];
+
+        for (let i = 1; i <= data.length; i++) {
+            result[i - 1] = data[data.length - i];
+        }
+
+        console.log(result);
+        return data;
+    }
+
+// PRACTICE 5
+
+const lines = 5;
+let result = '';
+
+for (let i = 1; i < lines; i++) {
+    for (let j = 0; j < i; j++) {
+        result += '*';
+    }
+    result += '\n';
+}
+
+console.log(result);
