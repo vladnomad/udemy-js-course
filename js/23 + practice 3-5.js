05a0ede45b10a2a8ae898ddad2dc56c18590e85f
vladnomad <vlad.nmd@gmail.com>
2022-07-29 20:13:14 +0200

    practice 5

--- js/23 + practice 3-5.js
+++ js/23 + practice 3-5.js
@@ -137,8 +137,11 @@ function fifthTask() {
 const lines = 5;
 let result = '';
 
-for (let i = 1; i < lines; i++) {
-    for (let j = 0; j < i; j++) {
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += ' ';
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
         result += '*';
     }
     result += '\n';
