845a7378ee8c1a861dc35e4119e67201dc6ca5f5
vladnomad <vlad.nmd@gmail.com>
2022-08-21 11:11:10 +0200

    practice 8 task 2 complete

--- js/practice 8.js
+++ js/practice 8.js
@@ -11,7 +11,7 @@ function getTimeFromMinutes(time) {
     let minutesStr = "";
 
     hours = Math.floor(time / 60);
-    minutes = time - (hours * 60);
+    minutes = time - (hours * 60); // time % 60
 
     const hLastDigitStr = String(hours).slice(-1);
     const hLastDigitNum = Number(hLastDigitStr);
@@ -23,6 +23,22 @@ function getTimeFromMinutes(time) {
     } else {
         hoursStr = "часов";
     }
+
+/*     switch (hours) {
+        case 0: 
+            hoursStr = 'часов';
+            break;
+        case 1:
+            hoursStr = 'час';
+            break;
+        case 2:
+        case 3:
+        case 4:
+            hoursStr = 'часа';
+            break;
+        default:
+            hoursStr = 'часов';
+    } */
 
     const mLastDigitStr = String(minutes).slice(-1);
     const mLastDigitNum = Number(mLastDigitStr);
@@ -40,13 +56,65 @@ function getTimeFromMinutes(time) {
 
 getTimeFromMinutes(10521);
 
-
-// WORK IN PROGRESS >>>
-
 function findMaxNumber(a,b,c,d) {
     if (typeof(a,b,c,d) !== "number" || a,b,c,d === undefined || typeof(a,b,c,d) === "string") {
-        return console.log(0);
+        return 0;
+    } else {
+        return Math.max(a,b,c,d);
     }
+}
+
+findMaxNumber(8,28,23,26);
+
+/*     let array = [a,b,c,d];
+    let numbers = [a/d, b/d, c/d, d/d];
+    console.log(numbers);
+    for( var i = 0; i < numbers.length; i++){ 
+        if ( numbers[i] < 1) { 
+            numbers.splice(i, 1); 
+            i--;
+        }
+    }
+    console.log(numbers);
+
+    let numbersP = numbers.filter(checkGreater1);
+    function checkGreater1(number) {
+        return number >= 1;
+    }
+
+    console.log(numbersP);
+
+    for (let i = 0; i < numbersP.length; i++) {
+        let num = numbersP[i] / numbersP[numbersP.length - 1];
+         console.log(i);
+        console.log(numbersP[i]);
+        console.log(numbersP.length);
+        console.log(numbersP[numbersP.length - 1]);
+        console.log(num);
+
+        if (num < 1) {
+            console.log(i);
+            continue;
+        }
+
+        console.log(numbersP[i]);
+
+        for (let i = 0; i < array.length; i++) {
+            let answer = array[i] 
+            
+            let array = [a,b,c,d];
+            let numbers = [a/d, b/d, c/d, d/d];
+            let numbersP = numbers.filter(checkGreater1);
+        
+            function checkGreater1(number) {
+                return number > 1;
+            }
+
+            return answer;
+        }
+    }
+    
+    
     let w = (a-b) + (a-c) + (a-d);
     let x = (b-a) + (b-c) + (b-d);
     let y = (c-a) + (c-b) + (c-d);
@@ -56,18 +124,32 @@ function findMaxNumber(a,b,c,d) {
     console.log(array);
 
     for (let i = 0; i <= array.length; i++) {
+        console.log(newArray);
+        if (array[i] < 0) {
+            newArray = array.slice(i + 1, array.length);
+            console.log(newArray);
+        } else {
+            console.log(newArray);
+            continue;
+        }
+    }
+    console.log(newArray); 
+
+
+
+     for (let i = 0; i <= array.length; i++) {
         if (array[i] < 0) {
             newArray = array.slice(i + 1, array.length);
         } else {
             i++;
         }
     }
-    console.log(newArray);
+    console.log(newArray); 
 
 
 
-    /* return console.log(array); */
-    /*     if (x < 0) {
+     return console.log(array); 
+         if (x < 0) {
         array.slice(0);
     } else if (y < 0) {
         array.slice(0);
@@ -75,7 +157,5 @@ function findMaxNumber(a,b,c,d) {
         array.slice(0);
     } else {
         return console.log(d);
-    } */
-} 
-
-findMaxNumber(10,8,23,4);
+    } 
+ */
