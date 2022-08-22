2b7798f720f0993f3d95f83d9fc5f5f0e8bbd73c
vladnomad <vlad.nmd@gmail.com>
2022-08-22 10:57:10 +0200

    lectures/part-31

--- js/lectures/part-31.js
+++ js/lectures/part-31.js
@@ -1,2 +1,31 @@
 "use strict";
 
+function first() {
+    // Do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}.`);
+    callback();
+}
+/* 
+learnJS("JavaScript", function() {
+    console.log("Я прошел этот урок!");
+});
+ */
+function done() {
+    console.log("Я прошел этот урок!");
+}
+
+learnJS("JavaScript", done);
+
