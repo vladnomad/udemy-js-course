9b03d28bc1e63e5f4bd984b652e8f765df6a0e53
vladnomad <vlad.nmd@gmail.com>
2022-09-02 19:00:52 +0200

    task-15 complete

---
+++ js/tasks/task-15.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const restaurantData = {
+    menu: [ 
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
+        }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
+
+function isOpen(prop) {
+    let answer = '';
+    prop ? answer = 'Open' : answer = 'Closed';
+
+    return answer;
+}
+
+console.log(isOpen(restaurantData.openNow));
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+    if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < +average.slice(0,-1)) {
+        return 'Price is lower than average';
+    } else {
+        return 'Price is higher than average';
+    }
+}
+
+console.log(isAverageLunchPriceTrue(restaurantData.menu[0], restaurantData.menu[1], restaurantData.averageLunchPrice));
+
+function transferWaitors(data) {
+    const copy = Object.assign({}, data);
+
+    copy.waitors = [{name: 'Mike', age: 32}];
+    return copy;
+}
+
+transferWaitors(restaurantData);
