c4447b9bcf7ebe747717242f2e0fd005f2425d9d
vladnomad <vlad.nmd@gmail.com>
2022-09-02 07:26:09 +0200

    lectures/part-40

---
+++ js/lectures/part-40.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/* 
+let number = 5; debugger
+
+function logNumber() {
+    let number = 4; debugger
+    console.log(number);
+}
+
+number = 6;
+
+logNumber(); debugger
+
+number = 8;
+
+logNumber(); debugger 
+*/
+
+function createCounter() {
+    let counter = 0;
+
+    const myFunction = function() { debugger
+        counter = counter + 1; debugger
+        return counter; debugger
+    };
+
+    return myFunction;
+}
+ debugger
+const increment = createCounter(); debugger
+const c1 = increment(); debugger
+const c2 = increment(); debugger
+const c3 = increment(); debugger
+
+console.log(c1, c2, c3);
