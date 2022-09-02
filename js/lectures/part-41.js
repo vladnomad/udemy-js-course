4c3bcb8a763de9d7ed16aad230706a5da44cc052
vladnomad <vlad.nmd@gmail.com>
2022-09-02 08:30:04 +0200

    lectures/part-41

---
+++ js/lectures/part-41.js
@@ -0,0 +1,35 @@
+// 1) let x = 5; alert( x++ );
+5
+
+// 2) [ ] + false - null + true;
+/* null */ NaN
+
+// 3) let y = 1; let x = y = 2; alert(x);
+2
+
+// 4) [ ] + 1 + 2;
+/* NaN */ "12"
+
+// 5) alert( "1"[0] );
+/* "10" */ "1" ? 1
+
+// 6) 2 && 1 && null && 0 && undefined;
+null
+
+// 7) !!( a && b ) и (a && b);
+/* true */ false
+
+// 8) alert( null || 2 && 3 || 4 );
+/* false */ 3
+
+// 9) a = [1, 2, 3]; b = [1, 2, 3]; a == b
+/* true */ false
+
+// 10) alert( +"Infinity" );
+Infinity /* typeof(Number) */
+
+// 11) "Ёжик" > "яблоко";
+false
+
+// 12) 0 || "" || 2 || undefined || true || falsе;
+2
