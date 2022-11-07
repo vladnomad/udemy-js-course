a7d3e902968e4f314ad1679e19546c5d234a53d1
vladnomad <vlad.nmd@gmail.com>
2022-11-07 07:28:04 +0100

    lectures/part-64.65 complete

---
+++ js/lectures/part-64.65.js
@@ -0,0 +1,38 @@
+const btn = document.querySelector(".btn");
+let timerId,
+    i = 0;
+
+function animation() {
+    const element = document.querySelector(".box");
+    let position = 0;
+
+    const id = setInterval(frame, 10);
+    function frame() {
+        if (position == 300) {
+            clearInterval(id);
+        } else {
+            position++;
+            element.style.top = position + "px";
+            element.style.left = position + "px";
+        }
+    }
+}
+
+btn.addEventListener("click", animation);
+
+/* btn.addEventListener("click", () => {
+    timerId = setInterval(logger, 500);
+});
+
+function logger() {
+    if (i === 3) {
+        clearInterval(timerId);
+    }
+    console.log("text");
+    i++;
+}
+
+let id = setTimeout(function log() {
+    console.log("Hello");
+    id = setTimeout(log, 500);
+}); */
