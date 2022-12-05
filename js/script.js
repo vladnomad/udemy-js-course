1b863e83ba4f890e2cff7e158d4b9cceab2fda6f
vladnomad <vlad.nmd@gmail.com>
2022-12-05 19:31:55 +0100

    lectures/modules and anonymous functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const app = "123";
+
+const number = 1;
+
+(function(){
+    let number = 2;
+    console.log(number);
+    console.log(number + 3);
+}());
+
+console.log(number);
+
+const user = (function(){
+    const private = function() {
+        console.log("Private function expression");
+    };
+
+    return {
+        sayHello: private
+    };
+}());
+
+user.sayHello();
